b5b41f2e1e0a2d7e3a1bf07d6ef1136424ac7111
artem-trubin <artemtrubin.letters@gmail.com>
2024-01-04 16:12:49 +0200

    Added some wonky styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,109 @@
 import { useState } from 'react'
+import styled from 'styled-components'
 
-const Header = ({ text }) => {
-  return (
-    <h1>{text}</h1>
-  )
-}
+const FeedbackContainer = styled.div`
+  width: 300px;
+
+  padding: 10px;
+
+  border-radius: 15px;
+  border: solid 3px black;
+
+  position: absolute;
+  top: calc(50% - 100px);
+  left: calc(50% - 150px);
+
+  box-shadow: 15px 15px black;
+  background-color: white;
+`;
+
+const FeedbackButton = styled.button`
+  width: 70px;
+  height: 30px;
+
+  cursor: pointer;
+
+  border: 2px solid black;
+  border-radius: 5px;
+
+  background-color: ${props => props.$bgColor || 'black'};
+  color: white;
+
+  &:hover {
+    background-color: white;
+    color: black;
+  }
+
+  &:active{
+    background-color: grey;
+  }
+`;
+
+const ButtonBlock = styled.div`
+  width: 100%;
+
+  display: flex;
+  justify-content: space-around;
+`;
+
+const Header = styled.h1`
+  text-align: center;
+  font-size: 24px;
+  font-weight: 800;
+
+  margin: 15px 0px;
+`;
+
+const Statistic = styled.div`
+  text-align: center;
+  font-size: 24px;
+
+  color: ${props => props.$status > 0 ? "green" : props.$status < 0 ? "red" : "yellow"}
+`;
 
 const App = () => {
   const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
+  const [ok, setok] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const all = good + neutral + bad;
+  const overall = good * 2 + ok + bad * -2;
+
+  const options = [
+    {
+      text: "GOOD",
+      onClick: () => setGood(good + 1),
+      bgColor: "#2f6924",
+    },
+    {
+      text: "OK",
+      onClick: () => setok(ok + 1),
+      bgColor: "#848733",
+    },
+    {
+      text: "BAD",
+      onClick: () => setBad(bad + 1),
+      bgColor: "#732626",
+    },
+  ];
 
   return (
-    <div>
-      <Header text={"Give feedback"} />
-      <div>
-        <button onClick={() => setGood(good + 1)}>Good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-        <button onClick={() => setBad(bad + 1)}>Bad</button>
-      </div>
-      <Header text={"Statistics"} />
-      <div>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {all}</p>
-        <p>average {all / 3}</p>
-        <p>positive {good / all * 100} %</p>
-      </div>
-    </div>
-  )
+    <FeedbackContainer>
+      <Header>GIVE FEEDBACK</Header>
+      <ButtonBlock>
+        {
+          options.map((option, i) => <FeedbackButton
+            $bgColor={option.bgColor}
+            onClick={option.onClick}
+            key={i}
+          >
+            {option.text}
+          </FeedbackButton>)
+        }
+      </ButtonBlock>
+      <Header>OVERALL</Header>
+      <Statistic $status={overall}>{overall}</Statistic>
+    </FeedbackContainer>
+  );
 }
 
 export default App
